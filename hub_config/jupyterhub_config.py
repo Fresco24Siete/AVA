4e98d46650ab724915f8d930c8c0962f49a1a1a5
FrescoDev <silver.bin.00011000@gmail.com>
2026-07-21 00:25:00 -0500

    fix route

--- hub_config/jupyterhub_config.py
+++ hub_config/jupyterhub_config.py
@@ -35,7 +35,7 @@ c.Spawner.auth_state_hook = auth_state_a_env
 
 
 c.JupyterHub.default_url = '/hub/spawn'
-c.Spawner.default_url = '/lab/tree/cuadernillo_poc_backend.ipynb?mode=single'
+c.Spawner.default_url = '/notebooks/cuadernillo_poc_backend.ipynb'
 
 # JupyterHub debe escuchar en todas las interfaces internas del contenedor
 c.JupyterHub.ip = '0.0.0.0'
